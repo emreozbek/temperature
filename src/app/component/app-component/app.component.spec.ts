a5db90039d0e7cdd4985ced4e2c7cef508da4de5
Emre OZBEK <accounts@emreozbek.net>
2018-07-10 00:33:53 +0300

    city list and history list called.

--- src/app/component/app-component/app.component.spec.ts
+++ src/app/component/app-component/app.component.spec.ts
@@ -1,13 +1,17 @@
-import { TestBed, async } from '@angular/core/testing';
+import {TestBed, async} from '@angular/core/testing';
 import {MatCardModule, MatTabsModule} from '@angular/material';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {CityListComponent} from '../city-list/city-list.component';
+import {HistoryListComponent} from '../history-list/history-list.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        CityListComponent,
+        HistoryListComponent
       ],
       imports: [
         MatCardModule,
