70cef60d3a05cf626d6a084a768ce85160648cfc
Emre OZBEK <accounts@emreozbek.net>
2018-07-12 00:18:46 +0300

    history list ui revised.

--- src/app/component/history-list/history-list.component.spec.ts
+++ src/app/component/history-list/history-list.component.spec.ts
@@ -4,6 +4,10 @@ import {MatFormFieldModule, MatIconModule, MatListModule, MatSelectModule} from 
 
 import { HistoryListComponent } from './history-list.component';
 import {CitiesService} from '../../service/cities/cities.service';
+import {DateUtilService} from '../../util/date/date.service';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {StoreModule} from '@ngrx/store';
+import {currentWeatherReducer} from '../../store/reducer/weather.reducer';
 
 describe('HistoryListComponent', () => {
   let component: HistoryListComponent;
@@ -12,8 +16,16 @@ describe('HistoryListComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ HistoryListComponent ],
-      imports: [BrowserAnimationsModule, MatFormFieldModule, MatSelectModule, MatIconModule, MatListModule],
-      providers: [CitiesService]
+      imports: [
+        BrowserAnimationsModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatIconModule,
+        MatListModule,
+        StoreModule.forRoot({weather: currentWeatherReducer})
+      ],
+      providers: [CitiesService, DateUtilService],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
--- src/app/component/history-list/history-list.component.ts
+++ src/app/component/history-list/history-list.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Store} from '@ngrx/store';
 import {CitiesService} from '../../service/cities/cities.service';
 import {environment} from '../../../environments/environment';
+import {WeatherStoreModel} from '../../model/weather/store.model';
+import {WeatherStore} from '../../store/store/weather.store';
+import {DateUtilService} from '../../util/date/date.service';
 
 @Component({
   selector: 'app-history-list',
@@ -8,14 +12,16 @@ import {environment} from '../../../environments/environment';
   styleUrls: ['./history-list.component.css']
 })
 export class HistoryListComponent implements OnInit {
-  symbol: string = environment.weather.symbol;
-
-  constructor(public cities: CitiesService) { }
-
-  ngOnInit() {
+  selected: string;
+  environment: Object = environment.weather;
+  weathers: WeatherStoreModel;
+  constructor(public cities: CitiesService,
+              public dateUtil: DateUtilService,
+              private store: Store<WeatherStore>) {
+    this.store.subscribe(state => {
+      this.weathers = state.weather;
+    });
   }
-
-  selectedCity() {
-
-  }
+  ngOnInit() {}
 }
+
