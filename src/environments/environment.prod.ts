b5c4f1a7ffcf10a3acb174758cb416661201aef0
Emre OZBEK <accounts@emreozbek.net>
2018-07-11 23:31:21 +0300

    parameter(cdn, iconExtension, refreshTime) added.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,10 @@ export const environment = {
       url: 'http://api.openweathermap.org/data/2.5/weather/',
       key: '05dea44a3b9a0f96bf3d862fcb033548'
     },
+    cdn: 'https://openweathermap.org/img/w/',
+    iconExtension: '.png',
     units: 'metric',
-    symbol: '°C'
+    symbol: '°C',
+    refreshTime: 1000 * 180
   }
 };
