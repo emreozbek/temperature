4d27ac533d2a0a5f3ad10346c15fb7edfde9071b
Emre OZBEK <accounts@emreozbek.net>
2018-07-13 23:04:05 +0300

    api url protocols changed to "https".

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   weather: {
     api: {
-      url: 'http://api.openweathermap.org/data/2.5/weather/',
+      url: 'https://api.openweathermap.org/data/2.5/weather/',
       key: '05dea44a3b9a0f96bf3d862fcb033548'
     },
     cdn: 'https://openweathermap.org/img/w/',
